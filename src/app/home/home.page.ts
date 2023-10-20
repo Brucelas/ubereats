5fa4cd42449573e816b7f52a47d9f2a672ea4ad5
Brucelas <127537816+Brucelas@users.noreply.github.com>
2023-10-19 22:03:48 -0300

    newnew

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,
+  public alertController: AlertController) {
 
 this.formularioLogin = this.fb.group({
   'nombre': new FormControl("", Validators.required),
@@ -19,5 +21,25 @@ this.formularioLogin = this.fb.group({
 
   }
 
+  ngOnInit() {
+  }
   
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('Ingresado');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos Incorrectos',
+        message: 'Los datos que ingreasste son incorrectos',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+  }
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,7 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) { 
+
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmarPassword': new FormControl("", Validators.required)
+    })
+
+  }
 
   ngOnInit() {
+  }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Datos Incompletos',
+        message: 'Faltan casillas por rellenar',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+
+    var usuario = {
+      nombre: f.nombre,
+      password: f.password
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
   }
 
 }
