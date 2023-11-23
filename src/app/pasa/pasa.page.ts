d57b07a32fa260f32c3ec845123f89c7545bdc37
coninini <co.arce@duocuc.cl>
2023-11-22 21:34:01 -0300

    intento5

--- src/app/pasa/pasa.page.ts
+++ src/app/pasa/pasa.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { GooglemapsComponent } from '../googlemaps/googlemaps.component';
 
 @Component({
   selector: 'app-pasa',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasaPage implements OnInit {
 
-  constructor() { }
+  usuario: any = {};
+
+  constructor(private modalController: ModalController) {} 
 
   ngOnInit() {
+  }
+  async addDirection() {
+    const ubicacion = this.usuario ? this.usuario.ubicacion : null;
+    let positionInput = {
+      lat: 0,
+      lng: 0,
+    };
+
+    if (ubicacion) {
+      positionInput = ubicacion;
+    }
+
+    const modalAdd = await this.modalController.create({
+      component: GooglemapsComponent,
+      animated: true,
+      mode: 'md',
+      componentProps: { position: positionInput },
+    });
+
+    await modalAdd.present();
+
+    const { data } = await modalAdd.onWillDismiss();
+
+    if (data && data.pos) {
+      this.usuario.ubicacion = data.pos;
+      console.log('this.usuario -> ', this.usuario);
+    }
   }
 
 }
